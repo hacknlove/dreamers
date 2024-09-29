b216f1dadc78ad2c5aca65f8066b12a75bab5da7
hacknlove <antonio@hacknlove.org>
2024-09-29 11:46:58 +0200

    added playwright

--- dreamers-web/src/app/dummy.test.ts
+++ dreamers-web/src/app/dummy.test.ts
@@ -1,7 +1,7 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect } from "vitest";
 
-describe('dummy', () => {
-  it('should pass', () => {
-    expect(true).toBe(true)
-  })
-})
+describe("dummy", () => {
+  it("should pass", () => {
+    expect(true).toBe(true);
+  });
+});
